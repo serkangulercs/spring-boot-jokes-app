fcbc142770fc44ceae1c7698c43ee1d14370c505
serkan <serkanguler.cs@gmail.com>
2021-05-15 17:14:11 +0300

    Joke page, controller and service modules has been implemented.

---
+++ src/main/java/com/springframework/jokesapp/controller/JokeController.java
@@ -0,0 +1,22 @@
+package com.springframework.jokesapp.controller;
+
+import com.springframework.jokesapp.service.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping("/")
+    public String getJoke(Model model) {
+        model.addAttribute("joke", jokeService.getRandomJoke());
+        return "index";
+    }
+}
---
+++ src/main/java/com/springframework/jokesapp/service/JokeService.java
@@ -0,0 +1,5 @@
+package com.springframework.jokesapp.service;
+
+public interface JokeService {
+    String getRandomJoke();
+}
